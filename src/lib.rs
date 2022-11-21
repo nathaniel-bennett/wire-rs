56bc9c9d8b34c2a9484a0116f03ca63f27d4fc05
Nathaniel Bennett <me@nathanielbennett.com>
2022-11-21 18:50:09 -0500

    Added a handful of basic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 // #![feature(array_try_from_fn)]
 
-
 mod reader;
 
-#[derive(Clone, PartialEq, Eq)]
+pub use reader::*;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
 #[non_exhaustive]
 pub enum WireError {
     InsufficientBytes,
@@ -12,17 +13,95 @@ pub enum WireError {
     Internal,
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn simple_read_finalize() {
+        let bytes = [0x12, 0x34, 0x56, 0x78];
+        let mut r1 = WireReader::<true>::new(&bytes);
+        
+        let val1 = r1.read();
+        let val2 = r1.read();
+        let val3 = r1.read_and_finalize();
+
+        assert!(val1 == Ok(0x12u8));
+        assert!(val2 == Ok(0x34u8));
+        assert!(val3 == Ok(0x5678u16));
+    }
+
+    #[test]
+    fn read_finalize_insufficient_bytes() {
+        let bytes = [0x12, 0x34, 0x56, 0x78];
+        let mut r1 = WireReader::<true>::new(&bytes);
+        
+        let val1 = r1.read();
+        let val2 = r1.read();
+        let val3: Result<u32, WireError> = r1.read_and_finalize();
+
+        assert!(val1 == Ok(0x12u8));
+        assert!(val2 == Ok(0x34u8));
+        assert!(val3 == Err(WireError::InsufficientBytes));
+    }
+
+    #[test]
+    fn read_str() {
+        let bytes = [0x12, 0x34, 0x56, 0x78];
+        let mut r1 = WireReader::<true>::new(&bytes);
+        let str1 = r1.read_ref(4);
+
+        assert!(str1 == Ok("\x12\x34\x56\x78"));
+        assert!(r1.finalize() == Ok(()));
+    }
+
+    #[test]
+    fn read_nonutf8_str_fail() {
+        let bytes = [0x9a, 0xbc, 0xde, 0xf0];
+        let mut r1 = WireReader::<true>::new(&bytes);
+        let str1 = r1.read_ref::<str>(4);
+
+        assert!(str1.is_err());
+        assert!(match str1 {
+                Err(WireError::InvalidData(_)) => true,
+                _ => false,
+            }
+        );
+    }
+
+    struct CustomWireReadable<'a> {
+        a: u8,
+        b: &'a str,
+    }
+
+    impl<'a> CompWireReadable<'a> for CustomWireReadable<'a> {
+        fn from_wire_comp<const E: bool>(curs: &mut WireCursor<'a>) -> Result<Self, WireError> {
+            // curs needs some stronger functionality
+
+            let a = curs.get_readable::<u8, E>()?;
+            let str_len = curs.get_readable::<u16, E>()?;
+
+            Ok(CustomWireReadable {
+                a: a,
+                b: curs.get_readable_ref::<str, E>(str_len as usize)?,
+            })
+        }
+    }
+
+    #[test]
+    fn custom_wire_readable() {
+        let bytes = [0x9a, 0x00, 0x05, 0x65, 0x66, 0x67, 0x68, 0x69];
+        let c1: CustomWireReadable;
+        
+        // c1's lifetime must be bound to `bytes`, not `r1`, so this should be able to compile
+        {
+            let mut r1 = WireReader::<true>::new(&bytes);
+            c1 = r1.read_comp().unwrap_or(CustomWireReadable { a: 0, b: "" });
+
+            assert!(r1.is_empty())
+        }
+
+        assert!(c1.a == 0x9a);
+        assert!(c1.b == "\x65\x66\x67\x68\x69")
     }
 }
